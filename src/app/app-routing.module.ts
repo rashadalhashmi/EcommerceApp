70cb072b59c086689fda1b8dda314f6f6fd6f206
rashadalhashmi <86948752+rashadalhashmi@users.noreply.github.com>
2021-11-30 22:20:33 +0200

    add html form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'user',loadChildren:()=>import('../app/components/users/user-auth.module').then(m=>m.UserAuthModule)}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
