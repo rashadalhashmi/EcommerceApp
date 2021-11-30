a021a589f1783c2a311d8e903c9f37026b2d8671
rashadalhashmi <86948752+rashadalhashmi@users.noreply.github.com>
2021-11-30 22:23:03 +0200

    changes

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+
+function passwordMatch(c:AbstractControl):{[key:string]:boolean}|null{
+  const passwordControl=c.get('password');
+  const confirmPassword=c.get('confirmPassword');
+
+  if(passwordControl?.pristine||confirmPassword?.pristine){
+    return null ;
+  }
+
+  if(passwordControl?.value==confirmPassword?.value){
+    return null ;
+  }
+
+
+  return {'match':true};
+}
 
 @Component({
   selector: 'app-register',
@@ -7,9 +27,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  userForm:any;
+  constructor(
+         private formBuilder:FormBuilder,
+         private route :Router) {}
 
   ngOnInit(): void {
+
+  this.userForm= this.formBuilder.group({
+    firstName:['',[Validators.required,Validators.minLength(3)]],
+    lastName:['',[Validators.required,Validators.maxLength(50)]],
+    email:['',[Validators.required,Validators.email]],
+    passwordGroup:this.formBuilder.group({
+    password:['',[Validators.required,Validators.minLength(7)]],
+    confirmPassword:['',[Validators.required]]
+    },{validator: passwordMatch}),
+    sendCatalog:true,
+
+   })
+
   }
 
+  save(): void {
+
+    // this.user.id=Math.floor(Math.random()*1000)+"";
+    // this.user.firstName=this.userForm.get('firstName').value
+    // this.user.lastName=this.userForm.get('lastName').value;
+    // this.user.password=this.userForm.get('passwordGroup.password').value;
+    // this.user.username=this.userForm.get('email').value;
+    // this.user.Token="slfjk-sdfkjdkl55f-d45df4-dfllk"
+
+    // this.userHttp.createUser(this.user).subscribe(res=>{
+    //   this.route.navigate(['user/login'])
+    //     console.log(res);
+
+    // });
+  }
 }
--- src/app/components/users/user-auth.module.ts
+++ src/app/components/users/user-auth.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { AngularMaterialModule } from 'src/angular-material/angular-material.module';
+import {ReactiveFormsModule,FormsModule} from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router';
 
 
 
+const route :Routes =[
+  {path:"register" ,component:RegisterComponent}
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { AngularMaterialModule } from 'src/angular-material/angular-material.mod
   ],
   imports: [
     CommonModule,
-    AngularMaterialModule
+    RouterModule.forChild(route),
+    AngularMaterialModule,
+    ReactiveFormsModule,FormsModule
   ]
 })
 export class UserAuthModule { }
