a97e8620d9a9f3c56a8b4596c2e895e12e25c935
Alshaimaa Karam <alshaimaak.cs@gmail.com>
2021-12-01 23:14:42 +0200

    logic for Remember me in login Form

--- src/app/services/user-auth.service.ts
+++ src/app/services/user-auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IUser } from '../viewmodel/iuser';
@@ -8,13 +9,18 @@ import { IUser } from '../viewmodel/iuser';
   providedIn: 'root'
 })
 export class UserAuthService {
-  user:IUser = {} as IUser;
-  constructor(private httpService:HttpClient) { }
+  user: IUser = {} as IUser;
+  constructor(private httpService: HttpClient, private cookiesService: CookieService) { }
 
-  Login(userName:string, password:string)
-  {
+  Login(userName: string, password: string, rememberMe: boolean) {
     this.user.UserName = userName;
     this.user.Password = password;
+
+    if(rememberMe) {
+      this.cookiesService.set('userName', userName);
+      this.cookiesService.set('Password', password);
+    }
+
     const httpOption = {
       headers: new HttpHeaders({
         'content-type': 'Application/JSON'
