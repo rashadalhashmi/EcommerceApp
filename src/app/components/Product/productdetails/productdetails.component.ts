365058c97de8f626dfd15712738a32316b30790d
rashadalhashmi <86948752+rashadalhashmi@users.noreply.github.com>
2021-12-10 20:14:50 +0200

    changes#7

--- src/app/components/Product/productdetails/productdetails.component.ts
+++ src/app/components/Product/productdetails/productdetails.component.ts
@@ -10,42 +10,42 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductdetailsComponent implements OnInit {
   productSentedId: number = 0;
-  product: any;
+  // product: any;
   //for test
-  // product = {
-  //   "name": "A4TECH HeadphoneHS-19",
-  //   "id": 1,
-  //   "price": 11254,
-  //   "quantity": 5,
-  //   "description": "With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.",
-  //   "discount": 15,
-  //   "images": [
-  //     {
-  //       "imageURL": "assets/products/1.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/2.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/3.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/4.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/5.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/6.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/7.webp"
-  //     },
-  //     {
-  //       "imageURL": "assets/products/8.webp"
-  //     }
-  //   ]
-  // }
+  product = {
+    "name": "A4TECH HeadphoneHS-19",
+    "id": 1,
+    "price": 11254,
+    "quantity": 5,
+    "description": "With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.",
+    "discount": 15,
+    "images": [
+
+         "assets/products/1.webp"
+      ,
+
+         "assets/products/2.webp"
+      ,
+
+         "assets/products/3.webp"
+      ,
+
+         "assets/products/4.webp"
+      ,
+
+         "assets/products/5.webp"
+      ,
+
+         "assets/products/6.webp"
+      ,
+
+         "assets/products/7.webp"
+      ,
+
+         "assets/products/8.webp"
+
+    ]
+  }
   priceWithoutDiscount: number = 0;
   price: number = 0;
 
@@ -54,25 +54,26 @@ export class ProductdetailsComponent implements OnInit {
   @ViewChild("actv") imgActive!: ElementRef;
 
   constructor(private activeRoute: ActivatedRoute, private productService: ProductService) {
-    this.activeRoute.paramMap.subscribe(
-      (params) => {
-        this.productSentedId = Number(params.get("PID"));
-        this.productService.getProductByID(this.productSentedId).subscribe({
-          next: (product) => {
-            this.product = product.data;
-            this.priceWithoutDiscount = this.product.price;
-            this.price = this.priceWithoutDiscount - (this.priceWithoutDiscount * this.product.discount / 100);
-            console.log(product)
-          }
-        })
-      }
-    )
+    // this.activeRoute.paramMap.subscribe(
+    //   (params) => {
+    //     this.productSentedId = Number(params.get("PID"));
+    //     this.productService.getProductByID(this.productSentedId).subscribe({
+    //       next: (product) => {
+    //         this.product = product.data;
+    //         this.priceWithoutDiscount = this.product.price;
+    //         this.price = this.priceWithoutDiscount - (this.priceWithoutDiscount * this.product.discount / 100);
+    //         console.log(product)
+    //       }
+    //     })
+    //   }
+    // )
   }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(imgs: any) {
+
+  styleProductImage(imgs: any){
     let images = this.imgActive.nativeElement.children;
     for (let i = 0; i < images.length; i++) {
       images[i].children[0].style.border = "1px solid white";
