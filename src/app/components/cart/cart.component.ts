917885057e10d8255fe460dcaa4945d711516fe6
Alshaimaa Karam <alshaimaak.cs@gmail.com>
2021-12-17 20:43:34 +0200

    pagenation + add customerId to order

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
 import { map, tap } from 'rxjs';
 import { ICart } from 'src/app/model/ICartItem';
+import { UserAuthGuard } from 'src/app/security/user-auth.guard';
 import { CartService } from 'src/app/services/cartService/cart.service';
+import { UserAuthService } from 'src/app/services/user/user-auth.service';
 import { Iproduct } from 'src/app/viewmodel/product/iproduct';
 
 @Component({
@@ -12,7 +16,7 @@ import { Iproduct } from 'src/app/viewmodel/product/iproduct';
 export class CartComponent implements OnInit {
 
   cart:ICart={items:[],totalPrice:0}
-  constructor(private cartservice:CartService) { }
+  constructor(private cartservice:CartService, private authguard:UserAuthGuard, private activeroute:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.cartservice.cart.subscribe(cart=>this.cart=cart)
